e76382f559d1e54c0dea4570b803240747d27a98
shibil <shibi2772@gmail.com>
2023-10-09 16:00:03 +0530

    shibil banner updates

--- src/components/baneer/Banner.tsx
+++ src/components/baneer/Banner.tsx
@@ -1,39 +1,64 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import BannerImg from '../../../public/Img/NashPoint.jpg'
 import BannerImgOne from '../../../public/Img/istockphoto-1365550676-2048x2048.jpg'
+import BannerImgTwo from '../../../public/Img/tree-736885_1280.jpg'
 import Icon from '../../../public/svg/banner.svg'
 
+const slides = [BannerImg, BannerImgOne, BannerImgTwo];
+
 const Banner = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const slides = [
-    BannerImg,
-    BannerImgOne,
-    BannerImg,
-  ];
-
-  const goToSlide = (index: React.SetStateAction<number>) => {
-    setCurrentSlide(index);
-  };
-
-  const nextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentSlide(
-      (prevSlide) => (prevSlide - 1 + slides.length) % slides.length
-    );
-  };
+    const [currentSlide, setCurrentSlide] = useState<number>(0);
+    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+  
+    const goToSlide = (index: number) => {
+      setCurrentSlide(index);
+    };
+  
+    const nextSlide = () => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    };
+  
+    const prevSlide = () => {
+      setCurrentSlide(
+        (prevSlide) => (prevSlide - 1 + slides.length) % slides.length+1
+      );
+    };
+  
+    const startAutoSlide = () => {
+      if (!intervalId) {
+        const newIntervalId = setInterval(() => {
+          const nextSlideIndex = (currentSlide + 1) % slides.length;
+          setCurrentSlide(nextSlideIndex);
+        }, 3000); // Change slide every 5 seconds (adjust the duration as needed)
+  
+        setIntervalId(newIntervalId);
+      }
+    };
+  
+    const stopAutoSlide = () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+        setIntervalId(null);
+      }
+    };
+  
+    useEffect(() => {
+      startAutoSlide();
+      return () => {
+        stopAutoSlide();
+      };
+    }, [currentSlide]);
 
   return (
     <div
       id="default-carousel"
-      className="relative w-full "
+      className="relative w-full"
       data-carousel="slide"
+      onMouseEnter={stopAutoSlide}
+      onMouseLeave={startAutoSlide}
     >
       <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
         {slides.map((src, index) => (
@@ -76,8 +101,7 @@ const Banner = () => {
         onClick={prevSlide}
       >
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-        <Image src={Icon} alt="Mountain Landscape" />
-              
+          <Image src={Icon} alt="Mountain Landscape" />
           <span className="sr-only">Previous</span>
         </span>
       </button>
@@ -97,9 +121,9 @@ const Banner = () => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="m1 9 4-4-4-4"
             />
           </svg>
