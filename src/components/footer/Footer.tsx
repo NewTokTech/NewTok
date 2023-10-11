a0f1efc749690297f4d8a4797dd41b7fdc0d8127
miidlaj <mumidlaj@gmail.com>
2023-10-11 11:30:08 +0530

    midlaj: footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -209,6 +209,9 @@ const Footer = () => {
             </div>
           </div>
         </div>
+
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"></hr>
+
       </footer>
     </>
   );
