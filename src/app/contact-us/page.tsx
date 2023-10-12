3d4baf65a594d2f664800fffc8fee00558d9545c
miidlaj <mumidlaj@gmail.com>
2023-10-12 17:58:46 +0530

    midlaj:service page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,5 +1,6 @@
+"use client";
+
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 import { FaPhoneVolume, FaLocationDot } from "react-icons/fa6";
 import { GrMail } from "react-icons/gr";
@@ -13,25 +14,25 @@ const Contact = () => {
         <h4 className="text-secondary lg:text-[18px] mt-5 text-center font-inter font-semibold">
           CONTACT US
         </h4>
-        <h1 className="text-secondary lg:text-[30px] mt-5 text-center font-inter font-semibold">
+        <h1 className="text-secondary text-[30px] mt-5 text-center font-inter font-semibold">
           GET IN <span className="text-primary">TOUCH</span>
         </h1>
         <div className="flex justify-center">
-          <p className="px-auto text-secondary lg:text-[16px] mt-5 mb-5 text-center font-inter ">
+          <p className="px-10 lg:mx-64 text-secondary lg:text-[16px] mt-5 mb-5 text-center font-inter ">
             Don’t worry about it getting too long. We know your awesome ideas
-            take time to explain; the more we learn about it the better <br />{" "}
-            we can help you out. We would love to hear all about your project.
-            Connect with us through the contact form for a free initial <br />{" "}
-            project consultation. Contact us and see what .
+            take time to explain; the more we learn about it the better we can
+            help you out. We would love to hear all about your project. Connect
+            with us through the contact form for a free initial project
+            consultation. Contact us and see what .
           </p>
         </div>
       </div>
 
-      <div className="bg-white rounded-lg mx-32 relative sm:items-center sm:pt-0 ">
+      <div className="bg-white rounded-lg lg:mx-32 relative sm:items-center sm:pt-0 pb-5">
         <div className="flex flex-col md:flex-row items-center md:items-start ">
           <div className="w-full md:w-2/5 h-full my-auto">
             <div className="sm:rounded-lg bg-secondary text-white h-full relative py-16">
-              <div className="py-12 px-10">
+              <div className="pb-10 px-10">
                 <h1 className="text-[28px] font-semibold tracking-tight text-white">
                   Contact Information
                 </h1>
@@ -40,8 +41,8 @@ const Contact = () => {
                 </p>
               </div>
 
-              <div className="py-16 flex flex-col space-y-10 px-10">
-                <div className="flex items-center mt-8">
+              <div className="pt-20 flex flex-col space-y-10 px-10 pb-28">
+                <div className="flex items-center space-x-5 mt-8 font-thin">
                   <FaPhoneVolume />
                   <div className="ml-4 text-md tracking-wide">
                     <a href="tel:+917012935874" target="_blank">
@@ -49,15 +50,15 @@ const Contact = () => {
                     </a>
                   </div>
                 </div>
-                <div className="flex items-center mt-4">
+                <div className="flex items-center space-x-5 mt-4 font-thin">
                   <GrMail />
-                  <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                  <div className="ml-4 text-md tracking-wide w-40">
                     <a href="mailto:info@newtoktech.com" target="_blank">
                       info@newtoktech.com
                     </a>
                   </div>
                 </div>
-                <div className="flex items-center mt-2">
+                <div className="flex items-center space-x-5 mt-2 font-thin">
                   <FaLocationDot />
                   <div className="ml-4 text-md tracking-wide">
                     Newtok Technologies Pvt Ltd.
@@ -76,39 +77,41 @@ const Contact = () => {
               />
             </div>
           </div>
-          <div className="w-full md:w-3/5 p-4">
-            <form className="w-full py-40">
+          <div className="w-full md:w-3/5 px-4">
+            <form className="w-full py-40 relative">
               <Image
                 src={"/contact/send.svg"}
                 alt="send icon"
                 width={265}
                 height={218}
-                className="absolute top-0 right-0"
+                className="absolute lg:top-0 lg:rotate-0 hidden lg:right-0 "
               />
 
               <div className="flex justify-between">
                 <div className="flex flex-col relative z-0 w-full px-1">
                   <input
-                    type="name"
                     name="name"
+                    type="name"
                     id="name"
                     placeholder=""
                     className="w-100 mt-2 py-2 peer block w-full appearance-none border-0 border-b border-black bg-transparent text-sm focus:border-primary focus:outline-none focus:ring-0"
                   />
+
                   <label
                     htmlFor="name"
                     className="absolute top-2 z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-b duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-primary"
                   >
                     Full Name
                   </label>
-                </div>{" "}
+                </div>
+
                 <div className="flex flex-col relative z-0 w-full px-1">
                   <input
-                    type="name"
+                    type="text"
                     name="companyName"
                     id="companyName"
                     placeholder="Optional"
-                    className="w-100 mt-2 py-2 peer block w-full appearance-none border-0 border-b border-black bg-transparent text-sm focus:border-primary focus:outline-none focus:ring-0"
+                    className="w-100 mt-2 py-2 peer block w-full appearance-none border-0 border-b border-black bg-transparent text-sm focus:border-primary focus:outline-none focus:ring-0 placeholder-transparent  focus:placeholder-gray-500 focus:placeholder-opacity-100 transform transition-colors duration-300 ease-in-out"
                   />
                   <label
                     htmlFor="companyName"
@@ -139,12 +142,19 @@ const Contact = () => {
                   name="message"
                   rows={5}
                   placeholder="Write your message"
-                  className="w-100 mt-2 py-2 peer block w-full appearance-none border-0 border-b border-black bg-transparent text-sm focus:border-primary focus:outline-none focus:ring-0"
+                  className="w-100 mt-2 py-2 peer block w-full appearance-none border-0 border-b border-black bg-transparent text-sm focus:border-primary focus:outline-none focus:ring-0 placeholder-transparent focus:placeholder-gray-500 focus:placeholder-opacity-100 transform transition-colors duration-300 ease-in-out"
                 ></textarea>
+
+                <label
+                  htmlFor="message"
+                  className="absolute top-2 z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-b duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-primary"
+                >
+                  Message
+                </label>
               </div>
               <div className="flex justify-end">
                 <button
-                  type="submit"
+                  type="button"
                   className="md:px-10 bg-primary hover:bg-blue-dark text-white py-3 px-8 mt-3 hover:bg-white hover:text-primary border-primary border transition ease-in-out duration-300"
                 >
                   Send Message
---
+++ src/app/services/page.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import React from "react";
+
+const Services = () => {
+  return (
+    <>
+      <section className="w-screen mx-auto mt-36 mb-10">
+        <h1 className="text-secondary lg:text-[40px] mt-5 text-center font-semibold">
+          <span className="">About </span>
+          <span className="text-primary">us</span>
+        </h1>
+        <div className="flex justify-center">
+          <p className="text-secondary lg:text-[15px] mt-5 mb-5 text-center font-light max-w-screen-sm">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the an unknown printer took a galley
+          </p>
+        </div>
+
+        <div className="py-16 mx-10 rounded-lg my-auto bg-white about text-secondary">
+          <div className="m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+            <div className="space-y-6 md:space-y-0 lg:flex lg:justify-center md:gap-6 lg:items-center lg:gap-12">
+              {/* <ImageContainer /> */}
+              <Image
+                src={"/about/image4.svg"}
+                alt="about 1"
+                height={827}
+                width={518}
+              />
+              <div className="md:7/12 lg:w-6/12 p-10">
+                <h2 className="text-[18px] md:text-[24px] lg:text-[30px] font-semibold">
+                  Navigating Tomo
+                  <span className="text-primary">rrow{"'"}s Horizons: </span>
+                  <br />
+                  Crafting Digital E
+                  <span className="text-primary">xcellence at Newtok</span>
+                  <br />
+                  Technologies
+                </h2>
+                <p className="mt-6 text-[14px] font-[500] leading-5">
+                  Welcome to Newtok Technologies, where we redefine software
+                  solutions with innovation. Since our 2003 inception, we{"'"}ve
+                  crafted digital transformation for enterprises through custom
+                  application development and IT services. We{"'"}re not just
+                  developers; we{"'"}re your digital partners, dedicated to
+                  forging unique solutions that set you apart. Join us in
+                  exploring the limitless possibilities of technology. Welcome
+                  to Newtok Technologies, where digital excellence awaits.
+                </p>
+
+                <button
+                  type="button"
+                  className="mt-10 py-2.5 px-12 mr-2 mb-5 text-sm font-medium text-primary focus:outline-none bg-white  border border-primary border-5 hover:bg-primary  hover:text-white transform transition-colors duration-300 ease-in-out"
+                >
+                  MORE ABOUT US
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Services;
