b61cc87d86d510e48ade0346fc3ada0ae3e606e8
miidlaj <mumidlaj@gmail.com>
2023-10-17 20:58:32 +0530

    services image scale changes

--- src/components/servicePage/SingleContainer.tsx
+++ src/components/servicePage/SingleContainer.tsx
@@ -20,32 +20,32 @@ const SingleContainer = (props: ServiceContent) => {
   return (
     <>
     
-      <div className="relative  sxl:mx-52 ">
+      <div className="relative  xl:mx-52 ">
         <Image
           src={img}
           alt="about 1"
           width={687}
           height={498}
-          className={`scale-75 sxl:hidden block  sxl:scale-90 overflow-visible -mb-10`}
+          className={`scale-75 xl:hidden block  xl:scale-90 overflow-visible -mb-10`}
         />
-        <div className="sxl:mx-10 rounded-sxl bg-white about mx-5 sxl:min-w-[519px] sxl:min-h-[400px] text-secondary" style={{ boxShadow: "0px -3px 11px 0px rgba(0, 0, 0, 0.25) inset" }}>
-          <div className="m-auto px-6 md:px-12 sxl:px-6 sxl:my-16">
-            <div className={`sxl:space-y-6 md:space-y-0 sxl:flex  ${position === 'left' ? 'sxl:justify-end' : 'sxl:justify-start'} md:gap-6 sxl:items-center sxl:gap-12`}>
+        <div className="xl:mx-10 rounded-xl bg-white about mx-5 xl:min-w-[519px] xl:min-h-[400px] text-secondary" style={{ boxShadow: "0px -3px 11px 0px rgba(0, 0, 0, 0.25) inset" }}>
+          <div className="m-auto px-6 md:px-12 xl:px-6 xl:my-16">
+            <div className={`xl:space-y-6 md:space-y-0 xl:flex  ${position === 'left' ? 'xl:justify-end' : 'xl:justify-start'} md:gap-6 xl:items-center xl:gap-12`}>
               <Image
                 src={img}
                 alt="about 1"
                 width={600}
                 height={498}
-                className={`absolute  bottom-0 sxl:block hidden scale-75 sxl:scale-100 overflow-visible ${position === 'left' ? '-left-4 ' : 'right-0'}`}
+                className={`absolute  bottom-0 xl:block hidden scale-75 sxl:scale-100 overflow-visible ${position === 'left' ? '-left-4 ' : 'right-0'}`}
               />
 
-              <div className="md:7/12 sxl:w-6/12 p-10  z-[999]">
+              <div className="md:7/12 xl:w-6/12 p-10  z-[999]">
 
                 <h2 className="text-[30px] font-semibold ">
                   {HOne} <span className="text-primary"> {HTwo}</span>
                 </h2>
 
-                <div className="flex flex-col sxl:justify-center sxl:align-center sxl:items-center text-secondary relative">
+                <div className="flex flex-col xl:justify-center xl:align-center xl:items-center text-secondary relative">
                   {text.map((value, index) => {
                     return (
                       <div className="" key={value.H1}>
