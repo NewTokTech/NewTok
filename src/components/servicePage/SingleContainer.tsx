4ddcd85f69752ef502f3d191065f21564c042095
shibil <shibi2772@gmail.com>
2023-10-17 20:10:34 +0530

    shibil:bugs fix

--- src/components/servicePage/SingleContainer.tsx
+++ src/components/servicePage/SingleContainer.tsx
@@ -25,22 +25,22 @@ const SingleContainer = (props: ServiceContent) => {
         <Image
           src={img}
           alt="about 1"
-          width={500}
+          width={687}
           height={498}
-          className={`scale-75 lg:hidden block  xl:scale-90 overflow-visible -mb-10`}
+          className={`scale-75 xl:hidden block  xl:scale-90 overflow-visible -mb-10`}
         />
-        <div className="xl:mx-10 rounded-xl bg-white about mx-5 lg:min-w-[519px] xl:min-h-[400px] text-secondary" style={{ boxShadow: "0px -3px 11px 0px rgba(0, 0, 0, 0.25) inset" }}>
-          <div className="m-auto px-6 md:px-12 xl:px-6 lg:my-16">
-            <div className={`lg:space-y-6 md:space-y-0 lg:flex  ${position === 'left' ? 'lg:justify-end' : 'lg:justify-start'} md:gap-6 lg:items-center xl:gap-12`}>
+        <div className="xl:mx-10 rounded-xl bg-white about mx-5 xl:min-w-[519px] xl:min-h-[400px] text-secondary" style={{ boxShadow: "0px -3px 11px 0px rgba(0, 0, 0, 0.25) inset" }}>
+          <div className="m-auto px-6 md:px-12 xl:px-6 xl:my-16">
+            <div className={`xl:space-y-6 md:space-y-0 xl:flex  ${position === 'left' ? 'xl:justify-end' : 'xl:justify-start'} md:gap-6 xl:items-center xl:gap-12`}>
               <Image
                 src={img}
                 alt="about 1"
                 width={600}
                 height={498}
-                className={`absolute  bottom-0 lg:block hidden z-40 scale-75 xl:scale-100 overflow-visible ${position === 'left' ? '-left-4 ' : 'right-0'}`}
+                className={`absolute  bottom-0 xl:block hidden z-40 scale-75 xl:scale-100 overflow-visible ${position === 'left' ? '-left-4 ' : 'right-0'}`}
               />
 
-              <div className="md:7/12 lg:w-6/12 p-10 ">
+              <div className="md:7/12 xl:w-6/12 p-10 ">
 
                 <h2 className="text-[30px] font-semibold ">
                   {HOne} <span className="text-primary"> {HTwo}</span>
